44f5ab116d97ab6d99ee2753fa8cea0f56fccf4e
azer <azerprojects@gmail.com>
2021-01-28 15:05:00 +0700

    + gcc compiler

---
+++ src/api/gcc.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const shell = require('child_process');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  shell.exec(
+    'gcc --version',
+    {
+      encoding: 'ascii',
+      timeout: 1000,
+    },
+    (e, stdout, stderr) => {
+      if (!e) {
+        // it's wokring!!! 😀
+        if (stdout.length !== 0) {
+          res.status(200).json({
+            message: stdout,
+          });
+        } else {
+          res.status(501).json({
+            error: stderr,
+          });
+        }
+      } else {
+        res.status(500).json({
+          error: {
+            code: e.code,
+            message: e.message,
+          },
+        });
+      }
+    }
+  );
+});
+
+module.exports = router;
