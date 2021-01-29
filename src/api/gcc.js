b6a59d11c9e95bb796eb505c0e9287225f989a31
azer <azerprojects@gmail.com>
2021-01-29 15:12:40 +0700

    + gcc version only

--- src/api/gcc.js
+++ src/api/gcc.js
@@ -2,31 +2,56 @@ const express = require('express');
 
 const shell = require('child_process');
 
+// const base64 = require('base-64');
+
+// const fs = require('fs');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  shell.exec('gcc --version', (e, stdout, stderr) => {
+    if (!e) {
+      res.status(200).json({
+        stdout,
+        stderr,
+      });
+    } else {
+      res.status(500).json({
+        error: {
+          ...e,
+          message: e.message,
+        },
+      });
+    }
+  });
+});
+
+/*
+router.post('/', (req, res) => {
+  // get content from body
+  const content = base64.decode(req.body.base64_content);
+
+  // write to file
+  fs.writeFileSync('input.c', content);
+
+  // use written file for gcc
+  const cmd = 'gcc input.c -o output';
   shell.exec(
-    'gcc --version',
+    cmd,
     {
-      encoding: 'ascii',
       timeout: 1000,
     },
     (e, stdout, stderr) => {
       if (!e) {
         // it's wokring!!! 😀
-        if (stdout.length !== 0) {
-          res.status(200).json({
-            message: stdout,
-          });
-        } else {
-          res.status(501).json({
-            error: stderr,
-          });
-        }
+        res.status(200).json({
+          stdout,
+          stderr,
+        });
       } else {
-        res.status(500).json({
+        res.status(400).json({
           error: {
-            code: e.code,
+            ...e,
             message: e.message,
           },
         });
@@ -34,9 +59,6 @@ router.get('/', (req, res) => {
     }
   );
 });
-
-router.post('/', (req, res) => {
-  res.send(req.body);
-});
+*/
 
 module.exports = router;
