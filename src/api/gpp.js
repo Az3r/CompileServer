0f32734a8ccc5bfd71fbbf8e48a071b61c84eeb3
azer <azerprojects@gmail.com>
2021-01-29 15:34:00 +0700

    + compilers version

---
+++ src/api/gpp.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const shell = require('child_process');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  shell.exec('g++ --version', (e, stdout, stderr) => {
+    if (!e) {
+      res.status(200).json({
+        stdout,
+        stderr,
+      });
+    } else {
+      res.status(500).json({
+        error: {
+          ...e,
+          message: e.message,
+        },
+      });
+    }
+  });
+});
+
+module.exports = router;
--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 
 const gcc = require('./gcc');
+const gpp = require('./gpp');
+const dotnet = require('./dotnet');
+const javac = require('./javac');
+const javascript = require('./javascript');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({
+  res.status(200).json({
     message: 'API - 👋🌎🌍🌏',
   });
 });
 
 router.use('/c', gcc);
+router.use('/cpp', gpp);
+router.use('/java', javac);
+router.use('/csharp', dotnet);
+router.use('/js', javascript);
 
 module.exports = router;
